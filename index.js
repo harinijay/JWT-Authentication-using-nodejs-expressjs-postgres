01fb8deebee6f28940fd02c72ca7ae719d8b18e8
harijayk <2020cs088@stu.ucsc.cmb.ac.lk>
2023-08-07 22:19:40 +0530

    database creation

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import {dirname,join} from 'path';
 import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {credentials:true, origin:process.env.URL || '*'};
+
+app.use(cors(corsOptions));
+app.use(json());
+app.use(cookieParser());
+
+app.use('/',express.static(join(__dirname,'public')));
+
+app.listen(PORT, ()=>console.log(`Server is listening on ${PORT}`));
