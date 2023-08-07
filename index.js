08c5b72ed7bc1165e836d43a58a8ba020f9207a5
harijayk <2020cs088@stu.ucsc.cmb.ac.lk>
2023-08-07 21:12:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import {dirname,join} from 'path';
+import { fileURLToPath } from 'url';
